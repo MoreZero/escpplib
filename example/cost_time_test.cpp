ac59b7e8e141801b0f85367ad5f833af87985c16
zero.li <1661023471@qq.com>
2018-01-07 12:06:17 +0800

    fix timer bug

--- example/cost_time_test.cpp
+++ example/cost_time_test.cpp
@@ -7,6 +7,6 @@ int main(){
     LOG_INFO("time cost test");
     escpplib::CCostTimer timer1("test1"); 
     escpplib::CCostTimer timer2(""); 
-
+    sleep(1);
     return 0;
 }
--- src/timer/timer.cpp
+++ src/timer/timer.cpp
@@ -23,9 +23,9 @@ int64_t CCostTimer::Cost() {
     gettimeofday(&end, NULL);
     begin_ = false;
     return ( 
-        (int64_t)end.tv_sec * 1000000 + (int64_t)end.tv_usec
+        ((int64_t)end.tv_sec * 1000000L + (int64_t)end.tv_usec)
         -
-        (int64_t)start_.tv_sec * 1000000 + (int64_t)start_.tv_usec
+        ((int64_t)start_.tv_sec * 1000000L + (int64_t)start_.tv_usec)
     );
 
 }
@@ -34,12 +34,14 @@ CCostTimer::~CCostTimer(){
     if (begin_){
         struct timeval end;
         gettimeofday(&end, NULL);
+        LOG_DEBUG("curret tiem: sec:%ld usec:%ld", end.tv_sec, end.tv_usec);
+        LOG_DEBUG("old tiem: sec:%ld usec:%ld", start_.tv_sec, start_.tv_usec);
 
         LOG_INFO("%s cost time:%ld[us]",
             msg_.c_str(),
-            (int64_t)end.tv_sec * 1000000 + (int64_t)end.tv_usec
+            ((int64_t)end.tv_sec * 1000000L + (int64_t)end.tv_usec)
             -
-            (int64_t)start_.tv_sec * 1000000 + (int64_t)start_.tv_usec);
+            ((int64_t)start_.tv_sec * 1000000L + (int64_t)start_.tv_usec));
     }
 }
 
